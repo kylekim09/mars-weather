e4a3ed457f3e97261bb0abf1ec148e1439b6fffb
Kyle Kim <kkim@mbp-kkim.fios-router.home>
2021-02-13 20:13:11 -0500

    add demo key and track progress for

--- src/utils/useMarsAPIHook.ts
+++ src/utils/useMarsAPIHook.ts
@@ -16,7 +16,7 @@ const parseData = (request:any): MarsDataList => {
 }
 
 const getMarsData = async (): Promise <MarsDataList>=> {
-    const marsApiResponse: Response = await fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY=json&ver=1.0');
+    const marsApiResponse: Response = await fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0');
     const response = await marsApiResponse.json();
     console.log(response);
     const parsedData:MarsDataList = parseData(response);
